d4d3fd9604922d8da8e263fba910a0068d919516
Kenneth Klette Jonassen <kenneth@bridgetech.tv>
2024-07-10 14:02:13 +0200

    build: bump version to 24.04-0.7.0.btech1
    Signed-off-by: Kenneth Klette Jonassen <kenneth@bridgetech.tv>

--- drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
+++ drivers/net/ethernet/mellanox/mlx5/core/mlx5_core.h
@@ -42,7 +42,7 @@
 #include <linux/mlx5/fs.h>
 #include <linux/mlx5/driver.h>
 
-#define DRIVER_VERSION	"24.04-0.7.0"
+#define DRIVER_VERSION	"24.04-0.7.0.btech1"
 
 extern uint mlx5_core_debug_mask;
 
